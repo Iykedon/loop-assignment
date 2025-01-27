af60aff0b4b5e67ba0df74663f79dec0a1886278
Iykedon <120271438+Iykedon@users.noreply.github.com>
2025-01-27 01:43:49 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// using i++(Post-increment)
+for(let i = 0; i < 9; i++){
+  console.log(i);  // In this example the current value of i is logged, then i is incremented.
+}
+
+// Using ++i(Pre-increment)
+
+for (let i = 0; i < 12; ++i){
+  console.log(i); // In this second example i is incremented first, then the updated value is logged. though the two outputs are the same.
+}
